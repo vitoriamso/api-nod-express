a30e437902fd46c7bb40a1d8649d1c360a8adc8d
Maria Vitória <133173885+vitoriamso@users.noreply.github.com>
2023-08-25 16:59:25 +0000

    MODIFICANDO A ESTRUTURA DOS ARQUIVOS

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import roteadorUsuario from "./routes/usuario.js";
+app.use(roteadorUsuario);
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API para CRUD usuario: link_gitgub",
+  });
+});
+
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
